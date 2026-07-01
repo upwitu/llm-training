f43532d33c12cc0215a0976854f8314d66321612
hungpv57 <phamhung1010.it@gmail.com>
2026-07-01 22:48:29 +0700

    fix tokenizer dependencies and improve ddp launch script

--- train.sh
+++ train.sh
@@ -1,3 +1,19 @@
-CUDA_VISIBLE_DEVICES=0,1 torchrun \
-    --nproc_per_node=2 \
+#!/bin/bash
+
+# Pick a free port automatically
+MASTER_PORT=$(python - <<'PY'
+import socket
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(("", 0))
+    print(s.getsockname()[1])
+PY
+)
+
+echo "Using master port: $MASTER_PORT"
+
+CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-1} \
+torchrun \
+    --nproc_per_node=${NPROC_PER_NODE:-1} \
+    --master_port=$MASTER_PORT \
     sft.py
